7855ee2bfc3c16bf873f701d8324dfc51ea7f5e0
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2023-07-15 00:22:37 +0530

    code modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const hbs = require("hbs")
+// const hbs = require("hbs")
 const LogInCollection = require("./mongodb")
 const port = process.env.PORT || 3000
 app.use(express.json())
